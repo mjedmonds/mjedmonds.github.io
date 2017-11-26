9209b0cd7e186d93a717d1d75e96d2674366efa7
Mark Edmonds <mark@mjedmonds.com>
2017-11-26 11:38:23 -0600

    added ch 16, 18, 19

--- CS80/notes_reveal/js/nav.js
+++ CS80/notes_reveal/js/nav.js
@@ -25,6 +25,9 @@ document.write('\
     <li><a href="ch12.5_jQuery.html">Ch 12.5. jQuery</a></li>\
     <li><a href="ch15_xml.html">Ch 15. XML</a></li>\
     <li><a href="ch15_xml.html">Ch 15.5. JSON</a></li>\
+    <li><a href="ch16_ajax.html">Ch 16. Ajax</a></li>\
+    <li><a href="ch18_databases.html">Ch 18. Databases</a></li>\
+    <li><a href="ch19_php.html">Ch 19. PHP</a></li>\
   </ul>\
 </li>\
 </ul>\
